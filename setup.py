5da8beff72b898b9111c4afb7545902d66e57133
rupanjali kumari <rupanjalikumari05@gmail.com>
2026-08-29 23:16:33 +0530

    setup file updated

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Sign-Language-Recognition',
+    version='0.1',
+    author="Bappy",
+    author_email="entbappy73@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
